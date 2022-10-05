4eb663daf0e591a71c5f1c0f4995d964131f4c94
Daniel <danielsottovia@gmail.com>
2022-10-05 11:28:12 -0300

    05.10.2022
    atualização às 11:26, com essas mudanças chegamos na nota 7, erros existentes: Teste 8: Verifica insercao de tipos de chamado com codigo duplicado
    [Falha critica!!!] Algo de errado ocorreu durante a execucao do seu codigo...
    #############################################################
    Teste 9: Verifica insercao de chamados duplicados
    [Falhou]
    -Obtido:
    " 4 "
    -Esperado:
    " 3 "
    --------------
    Teste 9 [Falhou!]
    O controlador esta permitindo a insercao de chamados duplicados, ou a contagem de chamados por tipo nao esta correta.
    Teste 12: Checagem de tipos na inclusao de chamados
    [Falhou]
    -Obtido:
    " 2 "
    -Esperado:
    " 0 "
    --------------
    [Falhou]
    -Obtido:
    " 1 "
    -Esperado:
    " 0 "
    --------------
    Teste 12 [Falhou!]
    Voce nao esta fazendo a checagem correta dos tipos dos parametros da funcao inclui_chamado

--- Exercicio6/controladorChamados.py
+++ Exercicio6/controladorChamados.py
@@ -14,24 +14,26 @@ class ControladorChamados(AbstractControladorChamados):
 
     def total_chamados_por_tipo(self, tipo: TipoChamado):
         cont = 0
+        # A contagem é na self.__lista_chamadas , não mexer mais
         for chamada in self.__lista_chamadas:
             if chamada.tipo == tipo:
                 cont += 1
         return cont
 
-    def inclui_chamado(self, data: Date, cliente: Cliente, tecnico: Tecnico, titulo: str, descricao: str, prioridade: int, tipo: TipoChamado):
+    def inclui_chamado(self, data: Date, cliente: Cliente, tecnico: Tecnico,
+                       titulo: str, descricao: str, prioridade: int, tipo: TipoChamado):
         chamada = Chamado(data=data, cliente=cliente, tecnico=tecnico, titulo=titulo
                           ,descricao=descricao, prioridade=prioridade, tipo=tipo)
-        for ind in self.__lista_chamadas:
+        '''for ind in self.__lista_chamadas:
             if ind.tipo.codigo == chamada.tipo.codigo:
-                return
+                return'''
         self.__lista_chamadas.append(chamada)
         return chamada
 
     def inclui_tipochamado(self, codigo: int, nome: str, descricao: str):
         tipo = TipoChamado(codigo=codigo, nome=nome, descricao=descricao)
-        for id in self.__lista_chamadas:
-            if id.tipo.codigo == tipo.codigo:
+        for id in self.__lista_tipo_chamadas:
+            if id.codigo == codigo:
                 return
         self.__lista_tipo_chamadas.append(tipo)
         return tipo
@@ -50,7 +52,29 @@ class ControladorChamados(AbstractControladorChamados):
         return cont
 
 def main():
+    caio = Cliente(nome='Caio', codigo=1234)
+    arthur = Tecnico(nome='Arthur', codigo= 9876)
+    teste = TipoChamado(codigo= 333, descricao='Teste', nome='Teste')
+    michel = Cliente(nome='Michel', codigo=5555)
+    reclama = TipoChamado(codigo=111, descricao='Reclamacao', nome='Reclamacao')
     daniel = ControladorChamados()
+    daniel.inclui_chamado(data = Date(2019,2,20) , cliente= caio,
+                          tecnico= arthur, titulo= 'Deus',
+                          descricao= 'Domingo', prioridade= 3,
+                          tipo= teste)
+    print(daniel.total_chamados_por_tipo(tipo= teste))
+    daniel.inclui_chamado(data=Date(2021, 7, 9), cliente= michel,
+                          tecnico= arthur, titulo= 'Segundo',
+                          descricao= 'Sábado', prioridade= 2,
+                          tipo= reclama)
+    print(daniel.total_chamados_por_tipo(tipo = teste))
+    print(daniel.total_chamados_por_tipo(tipo = reclama))
+    daniel.inclui_chamado(data = Date(2019,2,20) , cliente= caio,
+                          tecnico= arthur, titulo= 'Deus',
+                          descricao= 'Domingo', prioridade= 1,
+                          tipo= reclama)
+    print(daniel.total_chamados_por_tipo(tipo= reclama))
+
 
     pass
 
