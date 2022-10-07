78fa4dc51bf6474a2a0b2efd2d460061f1b4001a
Daniel <danielsottovia@gmail.com>
2022-10-07 17:27:24 -0300

    07/10
    Pós reunião com o professor.
    Dois erros principais, não li o texto direito onde ele dizia o que erra uma chamada duplicada.
     if ind.data == data and ind.cliente == cliente and ind.tecnico == tecnico and ind.tipo == tipo:
    
    E o não entendimento do que era a função tipos_chamados , a qual era um @property

--- Exercicio6/controladorChamados.py
+++ Exercicio6/controladorChamados.py
@@ -18,38 +18,40 @@ class ControladorChamados(AbstractControladorChamados):
         for chamada in self.__lista_chamadas:
             if chamada.tipo == tipo:
                 cont += 1
+
         return cont
 
-    def inclui_chamado(self, data: Date, cliente: Cliente, tecnico: Tecnico,
-                       titulo: str, descricao: str, prioridade: int, tipo: TipoChamado):
+    def inclui_chamado(self, data: Date, cliente: Cliente, tecnico: Tecnico, titulo: str, descricao: str,
+                       prioridade: int, tipo: TipoChamado):
+        if not (isinstance(data, Date) and isinstance(cliente, Cliente)
+                and isinstance(tecnico, Tecnico) and isinstance(titulo, str)
+                and isinstance(descricao, str) and isinstance(prioridade, int)
+                and isinstance(tipo,TipoChamado)):
+            return None
+
         chamada = Chamado(data=data, cliente=cliente, tecnico=tecnico, titulo=titulo
-                          ,descricao=descricao, prioridade=prioridade, tipo=tipo)
-        '''for ind in self.__lista_chamadas:
-            if ind.tipo.codigo == chamada.tipo.codigo:
-                return'''
+                          , descricao=descricao, prioridade=prioridade, tipo=tipo)
+        for ind in self.__lista_chamadas:
+            if ind.data == data and ind.cliente == cliente and ind.tecnico == tecnico and ind.tipo == tipo:
+                return None
         self.__lista_chamadas.append(chamada)
+
         return chamada
 
     def inclui_tipochamado(self, codigo: int, nome: str, descricao: str):
         tipo = TipoChamado(codigo=codigo, nome=nome, descricao=descricao)
         for id in self.__lista_tipo_chamadas:
-            if id.codigo == codigo:
+            if id.codigo == codigo and id.nome == nome and id.descricao == descricao:
                 return
         self.__lista_tipo_chamadas.append(tipo)
+
         return tipo
 
+    @property
     def tipos_chamados(self):
-        cont = 0
-        lista = []
-        for chamada in self.__lista_chamadas:
-            if len(lista) == 0:
-                lista.append(chamada.tipo)
-                cont += 1
-            else:
-                if chamada.tipo not in lista:
-                    cont += 1
-                    lista.append(chamada.tipo)
-        return cont
+        return self.__lista_tipo_chamadas
+
+
 
 def main():
     caio = Cliente(nome='Caio', codigo=1234)
@@ -74,6 +76,9 @@ def main():
                           descricao= 'Domingo', prioridade= 1,
                           tipo= reclama)
     print(daniel.total_chamados_por_tipo(tipo= reclama))
+    daniel.inclui_tipochamado(codigo= 333, descricao= 'Teste', nome='Teste')
+    daniel.inclui_tipochamado(codigo=111, descricao= 'Reclamacao', nome='Reclamacao')
+    daniel.inclui_tipochamado(codigo= 333, descricao= 'Teste', nome='Deus')
 
 
     pass
